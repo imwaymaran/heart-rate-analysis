219dc4b1a991edf8fc79ec92a8ba5a01ccaac9b6
Gennadii Ershov <gennadiialbershov@gmail.com>
2025-03-14 00:00:47 -0400

    feat: implement average function with documentation

--- metrics.py
+++ metrics.py
@@ -1,14 +1,25 @@
 def average(data: list) -> float:
     """
-    Calculate average value of the list.
+    Calculates average value of the list.
 
     Args:
-        data (list[int]): list of integers representing heart rate samples
+        data (list[int]): list of integers representing heart rate samples.
+        
     Returns:
-        float: a floating point value representing the average of this list
+        float: a floating point value representing the average of this list.
+        
+    Usage examples:
+        >>> average([80, 90, 100])
+        90.0
+
+        >>> average([60, 75, 90, 105])
+        82.5
     """
-    average = 0
-    ...
+    if not data:
+        return []
+    
+    average = sum(data) / len(data)
+    return round(average, 2)
 
 
 def maximum(data: list) -> float:
